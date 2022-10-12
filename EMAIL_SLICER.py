ffb97c57ba11dd3723b54ddc449b1e33976742df
MaheshKumarMK <maheshmkvb92@gmail.com>
2022-10-12 10:18:33 +0530

    Modified the .py file by adding the def function

--- EMAIL_SLICER.py
+++ EMAIL_SLICER.py
@@ -1,12 +1,13 @@
-import regex as re
-str1= input("Enter a valid email ID: ")
-pattern='^[a-zAA-Z0-9-_]+@[a-zA-Z0-9]+.[a-zA-Z]{1,3}$'
-if re.match(pattern, str1):
-    res=re.split(r"@",str1)
-    username, domain =res[0], res[1].split('.')[0]
-    print("Your username is {} and Domain is {}".format(username, domain))
-else:
-    print("Enter a valid email ID and try again!!")
+def email_slicer:
+    import regex as re
+    str1= input("Enter a valid email ID: ")
+    pattern='^[a-zAA-Z0-9-_]+@[a-zA-Z0-9]+.[a-zA-Z]{1,3}$'
+    if re.match(pattern, str1):
+        res=re.split(r"@",str1)
+        username, domain =res[0], res[1].split('.')[0]
+        print("Your username is {} and Domain is {}".format(username, domain))
+    else:
+        print("Enter a valid email ID and try again!!")
 
 
 
